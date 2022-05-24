b04dac252dbca99717805f25935030d694c77290
vzakharenkova <v.s.zakharenkova@gmail.com>
2022-05-24 19:19:02 +0300

    fix: fix 05 solution

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,21 +3,25 @@ const fsProm = require('fs').promises;
 const path = require('path');
 const pathCSS = path.join(__dirname, '/styles');
 const resultCSSPath = path.join(__dirname, '/project-dist', 'bundle.css');
-// const writeStream = fs.WriteStream(resultCSSPath, 'utf-8');
 
 joinCSS();
 
 async function joinCSS() {
-    const files = await fsProm.readdir(pathCSS, {
-        withFileTypes: true,
-      });
-      files.forEach(async (file, index) => {
-        if (file.isFile() && path.extname(file.name) === '.css') {
-          const filePath = path.join(__dirname, '/styles', file.name);
-          const readStream = fs.createReadStream(filePath, 'utf-8');
-          const writeStream = fs.createWriteStream(resultCSSPath, 'utf-8');
-          readStream.pipe(writeStream);
+  let writeStream = fs.WriteStream(resultCSSPath, 'utf-8');
+  const files = await fsProm.readdir(pathCSS, {
+    withFileTypes: true,
+  });
+  for await (file of files) {
+    if (file.isFile() && path.extname(file.name) === '.css') {
+      const filePath = path.join(__dirname, '/styles', file.name);
+      const stream = new fs.ReadStream(filePath, 'utf-8');
+      stream.on('readable', async () => {
+        let buffer = await stream.read();
+        if (buffer) {
+          const style = buffer.toString();
+          writeStream.write(`${style}\n\n`);
         }
       });
-
+    }
+  }
 }
